8025f71cb90902c84453eb53bfb2834e82ddf670
Cesar Torres <torres-cesar@outlook.com>
2022-06-21 14:30:34 -0500

    Create batchModel.js

---
+++ server/models/batchModel.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BatchSchema = new Schema({
+  id: {type: String, required: true},
+  material_id: {type: String, required: true},
+  material_name: {type: String, required: true},
+  location: {type: String, required: true},
+  client: {type: String, required: true},
+  quantity_total: {type: String, required: true},
+  date_recieve: {type: String, required: true},
+  date_expire: {type: String, required: true},
+  status: {type: String, required: true}
+});
+
+BatchSchema.virtual('url').get(function() {
+  return '/inventory/batch' + this._id;
+});
+
+module.exports = mongoose.model('Batch', BatchSchema);
