49c08c9330385e5320bca98303006b7e2ecded93
Cesar Torres <torres-cesar@outlook.com>
2022-06-21 18:24:13 -0500

    Setup router for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <nav>
+          <NavLink to='/home'>Home</NavLink>
+          <NavLink to='/inventory'>Inventory</NavLink>
+          <NavLink to='/performance'>Performance</NavLink>
+        </nav>
+      </header>
+      <main>
+
+      </main>
+
+      <Routes>
+        <Route path='/home'></Route>
+        <Route path='/inventory'></Route>
+        <Route path='/performance'></Route>
+      </Routes>
     </div>
   );
 }
