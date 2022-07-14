7d2c7312a861b229d8aac81a65d3524fc88b6940
Cesar Torres <torres-cesar@outlook.com>
2022-07-14 18:44:31 -0500

    Create test for rendering navbar

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -2,20 +2,32 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";  // optional
 import userEvent from "@testing-library/user-event";
-import App from '..//App.js';
+import App from '../App.js';
 import {BrowserRouter, MemoryRouter} from 'react-router-dom'
+
+const renderWithRouter = (ui, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+  return render(ui, { wrapper: BrowserRouter });
+}
 
 describe("App component", () => {
   describe("Home page", () => {
-    it.todo("should render Home component");
+    const { getByRole } = renderWithRouter(<App />, { route: '/home' });
+    it.todo("should appear at /home path");
+    it("should render Home component", () => {
+      expect(screen.getByRole('navigation')).toBeInTheDocument();
+    });
   })
   describe("Inventory page", () => {
+    it.todo("should appear at /inventory path");
     it.todo("should render Inventory component");
   })
   describe("Signup page", () => {
+    it.todo("should appear at /signup path");
     it.todo("should render SignUp component");
   })
   describe("Login page", () => {
+    it.todo("should appear at /login path");
     it.todo("should render Login component");
   })
   describe("Invalid page", () => {
