2351106193b7175dc8dc33150d5f48f7232dee4c
Cesar Torres <torres-cesar@outlook.com>
2022-07-20 14:08:37 -0500

    Rename route files

--- server/routes/auth.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var userController = require('../controllers/users')
-
-router.post('/login', userController.login);
-
-module.exports = router;
--- server/routes/batch.routes.js
+++ server/routes/batch.routes.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
-var batchController = require('../controllers/batches')
+var { batchController } = require('../controllers')
 
 router.get('/', batchController.readAllBatches);
 
