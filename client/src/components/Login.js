4ff8dd7e4a2631b90862f9a72991929cd4a8420a
Cesar Torres <torres-cesar@outlook.com>
2022-08-16 15:26:41 -0500

    Create login form card

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,22 @@
 export default function Login() {
   return (
-    <section>
-      Login
+    <section className="card bg-primary w-72 mt-14 self-center">
+      <div class="card-body">
+        <h2 class="card-title">Login</h2>
+        <form className="form-control">
+          <label className="label" htmlFor="username">
+            <span className="label-text">Username</span>
+          </label>
+          <input type="text" id="username" placeholder="Enter your username" className="input input-bordered"/>
+          <label className="label" htmlFor="password">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" id="password" placeholder="Enter your password" className="input input-bordered"/>
+        </form>
+        <div class="card-actions">
+          <button class="btn w-full">Login</button>
+        </div>
+      </div>
     </section>
   );
 }
